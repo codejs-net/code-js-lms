35b9bd424d9f041b711e7bd37b934728f2cdbe53
codejs-net <support@code-js.net>
2021-03-19 02:03:17 +0530

    Update pdf.php

--- config/pdf.php
+++ config/pdf.php
@@ -2,23 +2,28 @@
 
 return [
 	'mode'                  => 'utf-8',
-	'format'                => 'A4-L',
+	'format'                => 'A4',
+	'orientation'          	=> 'P',
 	'author'                => '',
 	'subject'               => 'LMS',
 	'keywords'              => '',
 	'creator'               => 'Code-js',
 	'display_mode'          => 'fullpage',
+	'margin_header'        	=> 0,
+	'margin_footer'        	=> 0,
 	'tempDir'               => base_path('../temp/'),
 	'font_path' => base_path('resources/fonts/'),
 	'font_data' => [
 		'abhayalibre' => [
 			'R'  => 'abhayalibreregular.ttf',
 			'B'  => 'abhayalibrebold.ttf',
+			'useOTL' 	=> 0xFF,
+			'useKashida' => 75,
 		],
 		'iskpota' => [
 			'R'  => 'iskpota.ttf',
 			'B'  => 'iskpotab.ttf',
-			'useOTL' => 0xFF,
+			'useOTL' 	=> 0xFF,
 			'useKashida' => 75,
 		],
 		'nirmala' => [
